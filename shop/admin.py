87ba974f3c41263cbc7a585c55cc0ddcc25206a9
YurimLee999 <67811883+YurimLee999@users.noreply.github.com>
2020-07-23 02:35:52 +0900

    13 모델을 통한 데이터 조회

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display=['pk', 'name', 'short_desc', 'price', 'is_publish']
+    list_display_links = ['name']
+    list_filter = ['is publish']
+    search_fields = ['name', 'updated_at']
+
+    def short_desc(self, item):
+        return item.desc[:20]
