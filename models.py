d2f2290bb7c6546434d16349cc0b57d42fceb400
YurimLee999 <67811883+YurimLee999@users.noreply.github.com>
2020-07-23 02:02:55 +0900

    12 장고 Admin을 통한 데이터 관리

--- models.py
+++ models.py
@@ -5,5 +5,9 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(black=True)
     price = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_publish = models.BooleanField(default = False)
+    created_at = models.DassteTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f'<{self.pk}>{self.name}'
